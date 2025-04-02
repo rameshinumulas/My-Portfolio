b3e20714a67eec1753874b81bc7de6cae43b9853
ramesh inumula <inumularamesh@gmail.com>
2025-04-02 21:10:36 +0530

    ui changes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,21 +7,20 @@ export default function SideBar() {
         alt='profile-avatar'
         className='avatar-sty'
       />
-      <div className='side-bar-details'>
-        <h3 className='text-style'>Ramesh Inumula</h3>
-        <p className='text-style-p'>Front-end Developer</p>
-      </div>
-      <div className='side-bar-details'>
-          <p>Email:</p>
-          <span>inumularamesh@gmail.com</span>
-      </div>
-      <div className='side-bar-details'>
-        <p>Phone:</p>
-        <span>+91 7285978169</span>
-      </div>
-      <div className='side-bar-details'>
-        <p>Address:</p>
-        <span>Visakhapatanam</span>
+      <div>
+        <div className='side-bar-details'>
+          <h3 className='text-style'>Ramesh Inumula</h3>
+          <p className='text-style-p'>Front-end Developer</p>
+        </div>
+        <div className='side-bar-details'>
+            <p>Email: inumularamesh@gmail.com</p>
+        </div>
+        <div className='side-bar-details'>
+          <p>Phone: +91 7285978169</p>
+        </div>
+        <div className='side-bar-details'>
+          <p>Address: Visakhapatanam Andhra Pradesh, 530002</p>
+        </div>
       </div>
     </div>
   )
